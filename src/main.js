e5e3ecef973c812e298f0eb27cacf5061d7b47f9
João Costa <joaovitorcostaoliveira@gmail.com>
2022-11-03 10:20:28 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -28,7 +28,7 @@ const securityCodeMasked = IMask(securityCode, securityCodePattern)
 
 const nameHolder = document.querySelector("#card-holder")
 const nameHolderPattern = {
-  mask: /^\p{L}+$/gu,
+  mask: /^\p{L}+$[" "]/gu,
 }
 
 const nameHolderMasked = IMask(nameHolder, nameHolderPattern)
